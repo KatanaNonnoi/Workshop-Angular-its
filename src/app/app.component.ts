94acac21ba11ab451c6574b61e96b6aff1a10da8
chanon_katana <chanon.dit@ku.th>
2022-11-29 23:53:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'workshop';
+
+
+  idm: number = 0;
+  geturl: any = Array;
+
+
+  constructor(private service : DataService){
+      
+  }
+ 
+ 
+
+
+
+ 
+
+  onNotifyClick(message: string): void{
+    this.geturl = message;
+  }
 }
